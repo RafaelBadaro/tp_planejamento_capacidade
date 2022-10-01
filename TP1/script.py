a1ab5d5379661e26cd209e19909cd28eebba4942
Rafael Badaró <rafabadaro.15@gmail.com>
2022-10-01 15:18:59 -0300

    Adicionando metodos para NumProcs e InterArrivalTime

--- TP1/script.py
+++ TP1/script.py
@@ -32,6 +32,7 @@ def calcular_estatisticas_gerais():
     print('InterArrivalTime:', round(interarrivaltime_coeficientevariacao, 2))
 
 
+# - Exe Time - 
 # Bloco 1, 0 <= x <= 50
 # Bloco 2, 51 <= x <= 150
 # Bloco 3, 151 <= x <= 350
@@ -45,9 +46,9 @@ def calcular_estatisticas_gerais():
 # Bloco 11, 51.151 <= x <= 102.350
 # Bloco 12, 102.351 <= x
 
-intervalos_menores = [0, 51, 151, 351, 751, 1551, 3151, 6351, 12751, 25551, 51151]
-intervalos_maiores = [50, 150 , 350, 750, 1550, 3150, 6350, 12750, 25550, 51150, 102350]
-def calcular_estatisticas_por_bloco():
+def calcular_estatisticas_por_bloco_ExeTime():
+    intervalos_menores = [0, 51, 151, 351, 751, 1551, 3151, 6351, 12751, 25551, 51151]
+    intervalos_maiores = [50, 150 , 350, 750, 1550, 3150, 6350, 12750, 25550, 51150, 102350]
     bloco = 1
     for x in range(12):
         print('-------------------------------')
@@ -75,6 +76,49 @@ def calcular_estatisticas_por_bloco():
         print('Coeficiente de variação: ', round(bloco_coeficientevariacao, 2))
         bloco += 1
 
+# - NumProcs - 
+# 32, 64, 128, 256, 512, 1024
+# Bloco 1, 0 <= x <= 32 
+# Bloco 2, 33 <= x <= 64
+# Bloco 3, 65 <= x <= 128
+# Bloco 4, 129 <= x <= 256
+# Bloco 5, 257 <= x <= 512
+# Bloco 6, 1024 <= x
+
+def calcular_estatisticas_por_bloco_NumProcs():
+    intervalos_menores = [0, 51, 151, 351, 751, 1551, 3151, 6351, 12751, 25551, 51151]
+    intervalos_maiores = [50, 150 , 350, 750, 1550, 3150, 6350, 12750, 25550, 51150, 102350]
+    bloco = 1
+    for x in range(6):
+        print('-------------------------------')
+        print('Bloco: ', bloco)
+        if bloco == 6:
+            print('Intervalo menor: 1024')
+            df_bloco = df.loc[(df['NumProcs'] >= 1024)]
+        else:
+            intervalo_menor = intervalos_menores[x]
+            intervalo_maior = intervalos_maiores[x]
+            print('Intervalo menor: ', intervalo_menor)
+            print('Intervalo maior: ', intervalo_maior)
+            df_bloco = df.loc[(df['ExeTime'] >= intervalo_menor) & (df['ExeTime'] <= intervalo_maior)]
+        
+        bloco_media = df_bloco['ExeTime'].mean()
+        bloco_desviopadrao = df_bloco['ExeTime'].std()
+        bloco_coeficientevariacao = (bloco_desviopadrao/bloco_media)
+
+        numero_processos_por_bloco = len(df_bloco.index)
+        print('Numero Processos:', numero_processos_por_bloco)
+        porcentagem = (numero_processos_por_bloco/numero_processos_total) * 100
+        print('Porcentagem de processos do total: ', round(porcentagem, 2))
+        print('Média: ', round(bloco_media, 2))
+        print('Desvio padrão: ', round(bloco_desviopadrao, 2))
+        print('Coeficiente de variação: ', round(bloco_coeficientevariacao, 2))
+        bloco += 1
+
+# - InterArrivalTime - 
+def calcular_estatisticas_por_bloco_InterArrivalTime():
+    intervalos_menores = [0, 51, 151, 351, 751, 1551, 3151, 6351, 12751, 25551, 51151]
+    intervalos_maiores = [50, 150 , 350, 750, 1550, 3150, 6350, 12750, 25550, 51150, 102350]
 
 
-calcular_estatisticas_por_bloco()
+calcular_estatisticas_por_bloco_ExeTime()
